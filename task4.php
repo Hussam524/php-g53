5ad648874bf9e4612c37e018733466bf94621e39
hussam524 <hussamhassan352@gmail.com>
2024-07-05 16:16:52 +0300

    tasks

---
+++ task4.php
@@ -0,0 +1,38 @@
+<?php
+
+
+//ternary operator
+$is_user_logged_in == true ;
+
+if($is_user_logged_in == true){
+    echo '<button> login </button>';
+}else{
+    echo' <button> logout </button>';
+}
+
+
+
+
+
+
+
+
+
+
+
+?>
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
